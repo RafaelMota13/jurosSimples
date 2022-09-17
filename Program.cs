4ca5782f92ef599216e9ec725abe819bd14e24f5
Rafael <faelmota7@gmail.com>
2022-09-17 11:03:40 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿decimal cap, tj, tmp, jur, mon;
+
+Console.Write("\nCapital (R$): ");
+cap =  Convert.ToDecimal(Console.ReadLine());
+Console.Write("Taxa de juros (%): ");
+tj = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Tempo (meses): ");
+tmp = Convert.ToDecimal(Console.ReadLine());
+
+jur = cap * (tj / 100) * tmp;
+
+mon = cap + jur;
+
+Console.WriteLine($"\nJuros R$): {jur}");
+Console.WriteLine($"Total (R$): {mon}\n");
